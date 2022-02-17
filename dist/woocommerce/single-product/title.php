bd67a80d77d5c53d13e6ea74d7e971d97ebb4132
Bence Agócs <bence@harom.studio>
2022-02-17 21:25:15 +0100

    update php

--- dist/woocommerce/single-product/title.php
+++ dist/woocommerce/single-product/title.php
@@ -19,4 +19,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-the_title( '<h1 class="product_title entry-title mb-3">', '</h1>' );
+the_title( '<h1 class="product_title entry-title mb-3 border-top">', '</h1>' );
