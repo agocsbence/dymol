0e27bb9c05709147ff661aa86b56821a626748d7
Bence Agócs <bence@harom.studio>
2022-02-17 13:53:15 +0100

    update php

--- src/front-page.php
+++ src/front-page.php
@@ -44,7 +44,7 @@ get_header();
 				while ( $loop->have_posts() ) : $loop->the_post();
 				global $product;
 				$id = get_the_ID();
-				$title = get_the_title();
+				$title = substr(get_the_title(), 0, 24);
             	$image = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'full', false ); ?>
 
 					<div class="card">
@@ -52,7 +52,7 @@ get_header();
 							<img src="<?php echo $image[0]; ?>" alt="<?php the_title(); ?>">
 							<div class="product-details">
 								<div class="product-data">
-									<div class="btn product-title"><?php echo substr($title, 0, 24); ?></div>
+									<div class="btn product-title"><?php echo $title; ?></div>
 									<br>
 									<div class="btn product-price"><?php echo $product->get_price_html(); ?></div>
 									<div class="product-variants">
