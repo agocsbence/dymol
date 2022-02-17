80236baf84931081099548acd222ef3869116b9b
Bence Agócs <bence@harom.studio>
2022-02-17 12:34:58 +0100

    first sitebuild

--- src/header.php
+++ src/header.php
@@ -22,7 +22,7 @@
     <meta name="msapplication-TileImage" content="<?php bloginfo('template_url') ?>/assets/icon/mstile-144x144.png">
 
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
-    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/assets/css/style.css?v=1.0.0">
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/assets/css/style.css?v=1.0.1">
 </head>
 
 <body <?php body_class(); ?>>
