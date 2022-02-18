d62929e78d1eef14f5601a36e592802edcc1a286
Bence Agócs <bence@harom.studio>
2022-02-18 08:51:05 +0100

    add bergyartas template

--- dist/page-bergyartas.php
+++ dist/page-bergyartas.php
@@ -18,10 +18,10 @@ get_header(); ?>
         <div class="bordered flex flex-column flex-row-center">
             <div class="grid grid-2">
                 <div class="textbox">
-                    <p>Amennyiben szeretne saját termékkört létrehozni, mi szívesen segítünk a megvalósításában a tervezéstől egészen a késztermék legyártásáig. Szabad kapacitásunk és szakmailag felkészült munkatársaink segítségével jelenleg is több cégnek gyártunk ilyen jellegű termékeket és termékcsoportokat háztartás-vegyi és kozmetikai termékkörön belül.</p>
+                    <p><?php the_filed('szoveg_bal'); ?></p>
                 </div>
                 <div class="textbox">
-                    <p>Ezzel kapcsolatos kérdéseivel kapcsolatban keressen Bennünket a 06-70-205-3111-es telefonszámon.</p>
+                    <p><?php the_filed('szoveg_jobb'); ?></p>
                 </div>
             </div>
             <img src="<?php bloginfo('template_url') ?>/assets/img/markak.png" alt="Márkák">
