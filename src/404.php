2112c5bfa0861ccbfd7bb11396c984f49a38a78a
Bence Agócs <bence@harom.studio>
2022-02-18 09:25:56 +0100

    update 404 page

--- src/404.php
+++ src/404.php
@@ -9,7 +9,7 @@ header( "refresh:5;url=" . $home );
 ?>
 
 <div id="error-404" class="container">
-    <div class="flex flex-row flex-row-start flex-space-between flex-m-column-reverse mt-5">
+    <div class="flex flex-row flex-row-start flex-space-between flex-m-column-reverse my-5">
         <div class="text-content">
             <h1 class="lead mb-2">404 - Hupsz!</h1>
             <h3 class="sub">Ez az oldal áthelyezésre került vagy pillanatnyilag nem elérhető. 5 másodpercen belül átirányítunk a kezdőoldalra!</h3>
