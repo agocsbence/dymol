9144dba74dfaec8cff95ea98e5d961b3c5b3d197
Bence Agócs <bence@harom.studio>
2022-02-24 22:55:25 +0100

    update php

--- src/page-mediatar.php
+++ src/page-mediatar.php
@@ -11,17 +11,19 @@ get_header(); ?>
         </div>
     </div>
     <div class="container">
-        <div class="grid grid-4 grid-gap-2">
-            <?php
-            
-            $katalogusok = get_field('katalogus');
-            if ($katalogusok) {
-                foreach($katalogusok as $katalogus) {
-                    echo "<a href=". $katalogus['katalogus_fajl'] ." target='blank'><img src=". $katalogus['kep']['url'] ." class='bordered' alt='Dymol katalógus'></a>";
+        <div class="mt-4">
+            <div class="grid grid-4 grid-gap-2">
+                <?php
+                
+                $katalogusok = get_field('katalogus');
+                if ($katalogusok) {
+                    foreach($katalogusok as $katalogus) {
+                        echo "<a href=". $katalogus['katalogus_fajl'] ." target='blank'><img src=". $katalogus['kep']['url'] ." class='bordered' alt='Dymol katalógus'></a>";
+                    }
                 }
-            }
 
-            ?>
+                ?>
+            </div>
         </div>
     </div>
 </section>
