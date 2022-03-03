3bd610905246ca9ed8b457374303628c66eab653
Bence <>
2022-03-03 12:33:53 +0100

    update mediatar page

--- dist/page-mediatar.php
+++ dist/page-mediatar.php
@@ -74,7 +74,7 @@ get_header(); ?>
 
 <script src="<?php bloginfo('template_url') ?>/assets/js/lightgallery.min.js"></script>
 <script type="text/javascript">
-    const galleries = document.querySelectorAll('gallery');
+    const galleries = document.querySelectorAll('.gallery');
     galleries.forEach(gallery => console.log(gallery.classList));
 
     lightGallery(document.getElementById('animated-thumbnials'), {
