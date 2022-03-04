f162d7e1d139912bb9838e9c4bce40779b837f7c
Bence <>
2022-03-04 10:59:06 +0100

    update featured filter on home page

--- dist/front-page.php
+++ dist/front-page.php
@@ -102,67 +102,51 @@ get_header();
 			<!-- end of loop -->
 		</div>
 	</div>
-</section>
+</section> 
+
 <section class="py-4">
 	<div class="container border-top">
 		<h2 class="section-title">Kiemelt termékek</h2>
 		<div class="grid grid-3 grid-gap-1 py-2">
-			<div class="card">
-				<a href="">
-					<img src="<?php bloginfo('template_url') ?>/assets/img/termek-3.png" alt="Dymosept">
-					<div class="product-details">
-						<div class="product-data">
-							<div class="btn product-title">Zum dishwasher</div>
-							<br>
-							<div class="btn product-price">8888Ft</div>
-							<div class="product-variants">
-								<div class="variant" style="background-color: #FEDE17;"></div>
-								<div class="variant" style="background-color: #52D3DB;"></div>
+			<!-- loop here -->
+			<?php
+			
+			$loop = new WP_Query( array( 'post_type' => 'product', 'posts_per_page' => 3, 'product_tag' => 'kiemelt') );
+			
+			if ( $loop->have_posts() ) :
+				while ( $loop->have_posts() ) : $loop->the_post();
+				global $product;
+				$id = get_the_ID();
+            	$image = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'full', false ); ?>
+
+					<div class="card">
+						<a href="<?php echo get_permalink(); ?>">
+							<img src="<?php echo $image[0]; ?>" alt="<?php the_title(); ?>">
+							<div class="product-details">
+								<div class="product-data">
+									<div class="btn product-title"><?php echo mb_strimwidth(get_the_title(), 0, 30, '...'); ?></div>
+									<br>
+									<div class="btn product-price"><?php echo $product->get_price_html(); ?></div>
+									<div class="product-variants">
+										<?php $colors = get_field('szinek');
+										if ($colors) {
+											foreach ($colors as $color) { ?>
+												<div class="variant" style="background-color: <?php echo $color['szin']; ?>;"></div>
+											<? }
+										}?>
+									</div>
+								</div>
 							</div>
-						</div>
+						</a>
 					</div>
-				</a>
-			</div>
-			<div class="card">
-				<a href="">
-					<img src="<?php bloginfo('template_url') ?>/assets/img/termek-4.png" alt="Zum kitchen">
-					<div class="product-details">
-						<div class="product-data">
-							<div class="btn product-title">Zum dishwasher</div>
-							<br>
-							<div class="btn product-price">8888Ft</div>
-							<div class="product-variants">
-								<div class="variant" style="background-color: #84B25D;"></div>
-								<div class="variant" style="background-color: #BC3F39;"></div>
-								<div class="variant" style="background-color: #FE7817;"></div>
-								<div class="variant" style="background-color: #6552DB;"></div>
-								<div class="variant" style="background-color: #426E54;"></div>
-								<div class="variant" style="background-color: #FEDE17;"></div>
-							</div>
-						</div>
-					</div>
-				</a>
-			</div>
-			<div class="card">
-				<a href="">
-					<img src="<?php bloginfo('template_url') ?>/assets/img/termek-4.png" alt="Zum dishwasher">
-					<div class="product-details">
-						<div class="product-data">
-							<div class="btn product-title">Zum dishwasher</div>
-							<br>
-							<div class="btn product-price">8888Ft</div>
-							<div class="product-variants">
-								<div class="variant" style="background-color: #84B25D;"></div>
-								<div class="variant" style="background-color: #BC3F39;"></div>
-								<div class="variant" style="background-color: #FE7817;"></div>
-								<div class="variant" style="background-color: #6552DB;"></div>
-								<div class="variant" style="background-color: #426E54;"></div>
-								<div class="variant" style="background-color: #FEDE17;"></div>
-							</div>
-						</div>
-					</div>
-				</a>
-			</div>
+
+				<?php 
+				endwhile;
+			endif;
+			wp_reset_postdata();
+			?>
+
+			<!-- end of loop -->
 		</div>
 	</div>
 </section>
