ff13beb88ffd5d250be2353d0882cfcfad3493b3
Bence <>
2022-03-04 12:22:55 +0100

    update search result layout

--- dist/page-mediatar.php
+++ dist/page-mediatar.php
@@ -47,8 +47,10 @@ get_header(); ?>
                         <div class="accordion mb-1">
                             <div class="accordion-header">
                                 <h1><?php echo $galeria['galeria_cim'] ?></h1>
-                                <span><?php echo $galeria['galeria_evszam'] ?></span>
-                                <span>+</span>
+                                <div class="right">
+                                    <span style="margin-right: 2rem;"><?php echo $galeria['galeria_evszam'] ?></span>
+                                    <span>+</span>
+                                </div>
                             </div>
                             <div class="accordion-content">
                                 <div class="text-block">
