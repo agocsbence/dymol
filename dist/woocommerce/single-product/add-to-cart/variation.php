0ea0f847f9b9d672bd8917df1e63a4dd36badcf1
Bence <>
2022-05-11 10:20:24 +0200

    update add-to-cart/variation.php to show price

--- dist/woocommerce/single-product/add-to-cart/variation.php
+++ dist/woocommerce/single-product/add-to-cart/variation.php
@@ -19,8 +19,8 @@ global $product;
 	<!-- <div class="woocommerce-variation-description">{{{ data.variation.variation_description }}}</div>
 	<div class="woocommerce-variation-price btn">{{{ data.variation.price_html }}}</div>
 	<div class="woocommerce-variation-availability">{{{ data.variation.availability_html }}}</div> -->
+	<div class="woocommerce-variation-price btn"><?php echo $product->get_price_html(); ?></div>
 </script>
-<div class="woocommerce-variation-price btn"><?php echo $product->get_price_html(); ?></div>
 <script type="text/template" id="tmpl-unavailable-variation-template">
 	<p><?php esc_html_e( 'Sorry, this product is unavailable. Please choose a different combination.', 'woocommerce' ); ?></p>
 </script>
